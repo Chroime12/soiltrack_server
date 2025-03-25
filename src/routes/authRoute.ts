0b69bacc0288d9f1baefc19ab25034ede17cc521
Hanz <hanzchrisromechico@gmail.com>
2025-03-26 00:35:41 +0800

    Try fixing forgot password

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -2,17 +2,26 @@ import { Router, Request, Response } from "express";
 
 const authRouter = Router();
 
-authRouter.get("/reset-password", (req: Request, res: Response) => {
-  const token = req.query.token as string;
+authRouter.get(
+  "/reset-wifi",
+  async (req: Request, res: Response): Promise<void> => {
+    console.log("🔄 Reset Request Sent");
+    try {
+      const token = req.query.token as string;
+      if (!token) {
+        res.status(400).json({ message: "Token is required" });
+      }
 
-  if (!token) {
-    res.status(400).json({ message: "Token is required" });
-    return;
+      const deepLink = `soiltrack://reset-password?token=${token}`;
+
+      res.json({
+        message: "Use this deep link on a mobile device",
+        deepLink,
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Error resetting device" });
+    }
   }
-
-  const deepLink = `soiltrack://reset-password?token=${token}`;
-
-  res.redirect(deepLink);
-});
+);
 
 export default authRouter;
